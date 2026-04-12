1eec9dc62adbcfdcf06a3608e46e9559517bb20a
Kalua <kalua@Kaluas-MacBook-Air.local>
2026-04-13 00:25:03 +0530

    improvement

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -9,7 +9,7 @@ const GptMovieSuggestions = () => {
   if (!movieNames) return null;
 
   return (
-    <div className="p-4 m-4 bg-black text-white bg-opacity-90">
+    <div className="p-4 m-4 bg-black text-white bg-opacity-80">
       {/* Show banner when serving persisted results */}
       {!isOnline && (
         <div className="mb-2 p-1 bg-yellow-600 rounded text-center font-bold">
